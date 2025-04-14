dd09348fe8123470d3e07a5b60e9349e46f21243
bbmisbah <misbah.bibi1203@gmail.com>
2025-04-14 14:36:44 +0200

    aggiunta nuova storia al componente Button (effetto disabled)

--- src/Button.stories.ts
+++ src/Button.stories.ts
@@ -4,7 +4,7 @@ import { Button } from './Button'
 
 const meta: Meta<typeof Button> = {
     component: Button,
-    title: 'Components/Button',
+    title: 'Components/Button', 
     tags: ['autodocs'], 
 };
 export default meta; 
@@ -15,6 +15,27 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
     args: {
         label: 'Click me', 
-        onClick: () => alert('Clicked!'),
+        onClick: async () => alert('Clicked!'),
     },
 }; 
+
+
+export const Disabled: Story = {
+    args: { 
+      label: 'Click me',
+      onClick: async () => {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+          alert('Button is enabled again!');
+      },
+      disabled: false,
+    },
+  };
+  
+  
+  
+  
+  
+  
+  
+
+
